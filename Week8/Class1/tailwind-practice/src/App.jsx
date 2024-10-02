43ea243b4417edccbc6c453489c5a5b21ad1adea
ArpitPatni <arpitpatni00@gmail.com>
2024-10-02 21:35:10 +0530

    Tailwind practice

---
+++ Week8/Class1/tailwind-practice/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='grid grid-cols-3'>
+        <div className='bg-red-50'>Hello</div>
+        <div className='bg-green-50'>Hello</div>
+        <div className='bg-yellow-50'>Hello</div>
+      </div>
+    </>
+  );
+}
+
+export default App
